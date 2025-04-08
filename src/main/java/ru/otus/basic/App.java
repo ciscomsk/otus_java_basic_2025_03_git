4801c00d7773f107f69f812e4a51c274f19c65e3
ciscomsk <kuznetzoff@gmail.com>
2025-04-08 09:29:43 +0300

    Task 1 completed [doSomething]

--- src/main/java/ru/otus/basic/App.java
+++ src/main/java/ru/otus/basic/App.java
@@ -1,7 +1,14 @@
 package ru.otus.basic;
 
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        doSomething();
+    }
+
+    public static void doSomething() {
+        IntStream.rangeClosed(1, 3).forEach(System.out::println);
     }
 }
