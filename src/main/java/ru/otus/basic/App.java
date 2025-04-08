e49a8a4e3b6ebed98e37b3222d042ff0811fa938
ciscomsk <kuznetzoff@gmail.com>
2025-04-08 09:30:39 +0300

    Task 2 completed

--- src/main/java/ru/otus/basic/App.java
+++ src/main/java/ru/otus/basic/App.java
@@ -5,10 +5,12 @@ import java.util.stream.IntStream;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        doSomething();
+        task2();
     }
 
-    public static void doSomething() {
-        IntStream.rangeClosed(1, 3).forEach(System.out::println);
+    public static void task2() {
+        System.out.println("---");
+        System.out.println("- -");
+        System.out.println("---");
     }
 }
